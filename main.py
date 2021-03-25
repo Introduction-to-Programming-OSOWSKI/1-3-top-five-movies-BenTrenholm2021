0670fed5dbce8d3cc5e1e543968fd77d1068e617
BenTrenholm2021 <814842@mystma.org>
2021-03-25 15:25:11 +0000

    firstTry

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #WRITE YOUR CODE IN THIS FILE
+
+def topFiveMovies(a, b, c, d, e):
+    return a + " " + b + " " + c + " " + d + " " + e
+a = The Titanic
+b = The Ring
+c = The Godfather
+d = 
+print(topFiveMovies("The Titanic" + " " + "The Ring" + " " + "The Godfather" + " " + "The Good Fellows" + " " + "Shrek"))
